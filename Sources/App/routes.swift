9ee018dda8211bd5b1d71d664a86e2d06bc4edb4
Christopher Liudahl-Rackley <clrminded@users.noreply.github.com>
2021-08-30 00:28:34 -0700

    added struct to route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,14 @@
 import Vapor
+import Tau
+
+struct FrontEndController {
+    func homeView(req: Request) throws -> EventLoopFuture<View> {
+    return req.tau.render(template: "index", context: [
+        "title": "myPage -Home",
+        "body": "Hi there, welcome!",
+        ])
+    }
+}
 
 func routes(_ app: Application) throws {
     app.get { req in
