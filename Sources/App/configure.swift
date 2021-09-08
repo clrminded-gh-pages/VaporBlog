388599c29f4340e211bf097580d4e7b2487acb18
Christopher Liudahl-Rackley <clrminded@users.noreply.github.com>
2021-09-07 23:08:19 -0700

    add fluent to config file

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,19 @@
 import Tau
 import Vapor
+import Fluent
+import FluentSQLiteDriver
 
 // configures your application
 public func configure(_ app: Application) throws {
+
+/// setup Fluent with a SQLite database under the Resources directory
+    let dbPath = app.directory.resourcesDirectory + "db.sqlite"
+    app.databases.use(.sqlite(.file(dbPath)), as: .sqlite)
+    
+
+
     // use the Public directory to serve public files
-    app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
     
     /// extend paths to always contain a trailing slash
@@ -42,4 +51,7 @@ public func configure(_ app: Application) throws {
     for router in routers {
         try router.boot(routes: app.routes)
     }
+    
+    /// use automatic database migration
+    try app.autoMigrate().wait()
 }
